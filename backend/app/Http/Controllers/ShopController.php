0f3699e01da65d2e8bffddb5d6bab0dbf9512a00
hasai <hasaikaz@gmail.com>
2021-03-01 18:07:43 +0900

    3章終了

--- backend/app/Http/Controllers/ShopController.php
+++ backend/app/Http/Controllers/ShopController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Stock; //追加
+
 use Illuminate\Http\Request;
 
 class ShopController extends Controller
 {
-    public function index() //追加
+    public function index() 
    {
-       return view('shop');
+        $stocks = Stock::Paginate(6); //Eloquantで検索
+        return view('shop',compact('stocks')); 
    }
     //
 }
