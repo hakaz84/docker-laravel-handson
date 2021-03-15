43208d88ac70548b4851259d23c1a939905cbe6d
hasai <hasaikaz@gmail.com>
2021-03-15 09:24:33 +0900

    4章終了

--- backend/resources/views/shop.blade.php
+++ backend/resources/views/shop.blade.php
@@ -7,7 +7,7 @@
            <h1 style="color:#555555; text-align:center; font-size:1.2em; padding:24px 0px; font-weight:bold;">商品一覧</h1>
            <div class="">
                <div class="d-flex flex-row flex-wrap">
-                   商品一覧を出したい
+                  
 
                     @foreach($stocks as $stock)
                         <div class="col-xs-6 col-sm-4 col-md-4 ">
